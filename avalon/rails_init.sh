a04035d70fa62ebd8a4db1778500808b4578bf1c
Phuong Dinh <phuongdh@gmail.com>
2018-11-05 15:50:52 -0500

    Run Passenger as user app

--- avalon/rails_init.sh
+++ avalon/rails_init.sh
@@ -1,26 +1,16 @@
 #!/bin/bash
 
-cd /home/app/avalonsolr/config
-zip -1 -r solr_hyrax_config.zip /home/app/avalon/solr/config/*
+zip -jrml solr_hyrax_config.zip /home/app/avalon/solr/config/*
 curl -H "Content-type:application/octet-stream" --data-binary @solr_hyrax_config.zip "http://solr:8983/solr/admin/configs?action=UPLOAD&name=avalon-config"
 curl -H 'Content-type: application/json' http://solr:8983/api/collections/ -d '{create: {name: avalon, config: avalon-config, numShards: 1}}'
 
-# sendmail needs this to work
-line=$(head -n 1 /etc/hosts)
-line2=$(echo $line | awk '{print $2}')
-echo "$line $line2.localdomain" >> /etc/hosts
-service sendmail start
-
 # batch ingest cronjob wouldn't autorun without this
 touch /var/spool/cron/crontabs/app
+#
+# BACKGROUND=yes QUEUE=* bundle exec rake resque:work
+# BACKGROUND=yes bundle exec rake environment resque:scheduler
+# RAILS_ENV=production bundle exec rake db:migrate
 
-chmod 0777 -R /masterfiles
-chown -R app /masterfiles
-cd /home/app/avalon
-
-su app
-BACKGROUND=yes QUEUE=* bundle exec rake resque:work
-BACKGROUND=yes bundle exec rake environment resque:scheduler
-RAILS_ENV=production bundle exec rake db:migrate
-exit
-
+setuser app bash -c "BACKGROUND=yes bundle exec rake environment resque:scheduler"
+setuser app bash -c "BACKGROUND=yes QUEUE=* bundle exec rake resque:work"
+setuser app bash -c "RAILS_ENV=production bundle exec rake db:migrate"
