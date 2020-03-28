b3b04218709be7159f9011053882368d102ed312
Douglas Hahn <dhahn@library.tamu.edu>
2020-03-28 15:56:58 -0500

    Update rails_init.sh

--- avalon/rails_init.sh
+++ avalon/rails_init.sh
@@ -15,25 +15,25 @@ service sendmail start
 touch /var/spool/cron/crontabs/root
 touch /var/spool/cron/crontabs/app
 
-chown -R root:1002 /mnt/avalon_dev/masterfiles/
-chmod -R 0777 /mnt/avalon_dev/masterfiles/
+chown -R app:1002 /mnt/avalon/masterfiles/
+chmod -R 0777 /mnt/avalon/masterfiles/
 
-chown -R root:1002 /mnt/avalon_dev/ingest/
-chmod -R 0777 /mnt/avalon_dev/ingest/
+chown -R app:1002 /mnt/avalon/ingest/
+chmod -R 0777 /mnt/avalon/ingest/
 
-chmod -R 0777 /mnt/avalon_dev/dropbox
-chown -R root:1002 /mnt/avalon_dev/dropbox
+chown -R app:1002 /mnt/avalon/dropbox
+chmod -R 0777 /mnt/avalon/dropbox
 
-chmod -R 0777 /mnt/avalon_dev/new_home/app/avalon/tmp
-chown -R root:1002 /mnt/avalon_dev/new_home/app/avalon/tmp
+chown -R app:1002 /mnt/avalon/home/app/avalon/tmp
+chmod -R 0777 /mnt/avalon/home/app/avalon/tmp
 
-echo "* * * * * /bin/bash -l -c '/mnt/avalon_dev/master_rights.sh' >/dev/null 2>&1" >> /var/spool/cron/crontabs/root
-echo "* * * * * /bin/bash -l -c '/mnt/avalon_dev/ingest_rights.sh' >/dev/null 2>&1" >> /var/spool/cron/crontabs/root
-echo "* * * * * /bin/bash -l -c '/mnt/avalon_dev/dropbox_rights.sh' >/dev/null 2>&1" >> /var/spool/cron/crontabs/root
-echo "* * * * * /bin/bash -l -c '/mnt/avalon_dev/tmp_rights.sh' >/dev/null 2>&1" >> /var/spool/cron/crontabs/root
+echo "* * * * * /bin/bash -l -c '/mnt/avalon/master_rights.sh' >/dev/null 2>&1" >> /var/spool/cron/crontabs/root
+echo "* * * * * /bin/bash -l -c '/mnt/avalon/ingest_rights.sh' >/dev/null 2>&1" >> /var/spool/cron/crontabs/root
+echo "* * * * * /bin/bash -l -c '/mnt/avalon/dropbox_rights.sh' >/dev/null 2>&1" >> /var/spool/cron/crontabs/root
+echo "* * * * * /bin/bash -l -c '/mnt/avalon/tmp_rights.sh' >/dev/null 2>&1" >> /var/spool/cron/crontabs/root
 
 chmod 600 /var/spool/cron/crontabs/root
 service cron reload
 
 cd /home/app/avalon
-su -m -c "bundle exec rake db:migrate"
+su -m -c "bundle exec rake db:migrate" app
