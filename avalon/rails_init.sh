386c9d94a24584dbb0d5efb5b5d89342dc9e4555
doug-hahn <dhahn@tamu.edu>
2020-06-11 07:36:17 -0500

    Update rails_init.sh

--- avalon/rails_init.sh
+++ avalon/rails_init.sh
@@ -11,6 +11,22 @@ cp -f /tmp/hosts.new /etc/hosts
 
 service sendmail start
 
+groupadd -g 1002 DIStaffUnix
+
+# Rotate Avalon logs
+echo "" >> /etc/logrotate.conf
+echo "########## Add Avalon Rotate ##########" >> /etc/logrotate.conf
+echo "/home/app/avalon/log/*.log {" >> /etc/logrotate.conf
+echo "  su app DIStaffUnix" >> /etc/logrotate.conf
+echo "  daily" >> /etc/logrotate.conf
+echo "  missingok" >> /etc/logrotate.conf
+echo "  rotate 7" >> /etc/logrotate.conf
+echo "  compress" >> /etc/logrotate.conf
+echo "  delaycompress" >> /etc/logrotate.conf
+echo "  notifempty" >> /etc/logrotate.conf
+echo "  copytruncate" >> /etc/logrotate.conf
+echo "}" >> /etc/logrotate.conf
+
 # batch ingest cronjob wouldn't autorun without this
 touch /var/spool/cron/crontabs/root
 touch /var/spool/cron/crontabs/app
