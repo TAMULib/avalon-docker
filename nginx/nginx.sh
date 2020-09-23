2620a21a2123739ca8c9b0aec8f5331b8b719c48
doug-hahn <dhahn@tamu.edu>
2020-09-23 07:34:55 -0500

    Bring Back into alignment with upstream

--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 
 # Make config file from template
-[ -z "$env_rtmp_avalon_url" ] && env_rtmp_avalon_url="http://avalon"
-[ -z "$env_avalon_streaming_port" ] && env_avalon_streaming_port=80
-[ -z "$env_master_file_location" ] && env_master_file_location=/data
-[ -z "$env_proxy_pass_ip" ] && env_proxy_pass_ip=127.0.0.1
-export env_rtmp_avalon_url
-export env_avalon_streaming_port
-export env_master_file_location
-export env_proxy_pass_ip
-envsubst '$env_rtmp_avalon_url,$env_avalon_streaming_port,$env_master_file_location,$env_proxy_pass_ip' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+[ -z "$AVALON_DOMAIN" ] && AVALON_DOMAIN="http://avalon"
+[ -z "$AVALON_STREAMING_PORT" ] && AVALON_STREAMING_PORT=80
+export AVALON_DOMAIN
+export AVALON_STREAMING_PORT
+envsubst '$AVALON_DOMAIN,$AVALON_STREAMING_PORT' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 exec /usr/local/nginx/sbin/nginx
