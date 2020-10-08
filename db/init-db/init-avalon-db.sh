2046367f776babf428ea94d0202e4c3987dd6d73
doug-hahn <dhahn@tamu.edu>
2020-10-08 09:38:38 -0500

    Setup initial user Doug will parameterize it later

--- db/init-db/init-avalon-db.sh
+++ db/init-db/init-avalon-db.sh
@@ -6,3 +6,9 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
   CREATE DATABASE avalon WITH ENCODING='UTF8';
   GRANT ALL PRIVILEGES ON DATABASE avalon TO $AVALON_DB_USER;
 EOSQL
+
+psql -v ON_ERROR_STOP=1 -d avalon --username "$POSTGRES_USER" <<-EOSQL
+INSERT INTO role_maps(entry, parent_id) VALUES ('cn=douglas hahn,ou=applications,ou=digital initiatives,ou=user services,ou=useraccounts,dc=library,dc=tamu,dc=edu', 1);
+INSERT INTO role_maps(entry, parent_id) VALUES ('cn=douglas hahn,ou=applications,ou=digital initiatives,ou=user services,ou=useraccounts,dc=library,dc=tamu,dc=edu', 3);
+INSERT INTO role_maps(entry, parent_id) VALUES ('cn=douglas hahn,ou=applications,ou=digital initiatives,ou=user services,ou=useraccounts,dc=library,dc=tamu,dc=edu', 5);
+EOSQL
