c0a17d7ae2ed3e0629533599f2b0e59fb779ead9
doug-hahn <dhahn@tamu.edu>
2020-10-13 08:19:16 -0500

    update docker

--- avalon/rails_init.sh
+++ avalon/rails_init.sh
@@ -27,29 +27,5 @@ echo "  notifempty" >> /etc/logrotate.conf
 echo "  copytruncate" >> /etc/logrotate.conf
 echo "}" >> /etc/logrotate.conf
 
-# batch ingest cronjob wouldn't autorun without this
-touch /var/spool/cron/crontabs/root
-touch /var/spool/cron/crontabs/app
-
-chown -R app:1002 /mnt/avalon/masterfiles/
-chmod -R 0777 /mnt/avalon/masterfiles/
-
-chown -R app:1002 /mnt/avalon/ingest/
-chmod -R 0777 /mnt/avalon/ingest/
-
-chown -R app:1002 /mnt/avalon/dropbox
-chmod -R 0777 /mnt/avalon/dropbox
-
-chown -R app:1002 /mnt/avalon/home/app/avalon/tmp
-chmod -R 0777 /mnt/avalon/home/app/avalon/tmp
-
-echo "* * * * * /bin/bash -l -c '/mnt/avalon/master_rights.sh' >/dev/null 2>&1" >> /var/spool/cron/crontabs/root
-echo "* * * * * /bin/bash -l -c '/mnt/avalon/ingest_rights.sh' >/dev/null 2>&1" >> /var/spool/cron/crontabs/root
-echo "* * * * * /bin/bash -l -c '/mnt/avalon/dropbox_rights.sh' >/dev/null 2>&1" >> /var/spool/cron/crontabs/root
-echo "* * * * * /bin/bash -l -c '/mnt/avalon/tmp_rights.sh' >/dev/null 2>&1" >> /var/spool/cron/crontabs/root
-
-chmod 600 /var/spool/cron/crontabs/root
-service cron reload
-
 cd /home/app/avalon
 su -m -c "bundle exec rake db:migrate" app
