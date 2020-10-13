9e31aff63d9f64acd23cb1b3b47a73c136bd8f1a
doug-hahn <dhahn@tamu.edu>
2020-10-13 14:06:45 -0500

    minor config tweaks

--- avalon/rails_init.sh
+++ avalon/rails_init.sh
@@ -27,5 +27,25 @@ echo "  notifempty" >> /etc/logrotate.conf
 echo "  copytruncate" >> /etc/logrotate.conf
 echo "}" >> /etc/logrotate.conf
 
+# batch ingest cronjob wouldn't autorun without this
+touch /var/spool/cron/crontabs/root
+touch /var/spool/cron/crontabs/app
+
+chown -R app:1002 /mnt/avalon/masterfiles/
+chmod -R 0777 /mnt/avalon/masterfiles/
+
+chown -R app:1002 /mnt/avalon/ingest/
+chmod -R 0777 /mnt/avalon/ingest/
+
+chown -R app:1002 /mnt/avalon/dropbox
+chmod -R 0777 /mnt/avalon/dropbox
+
+echo "* * * * * /bin/bash -l -c '/mnt/avalon/master_rights.sh' >/dev/null 2>&1" >> /var/spool/cron/crontabs/root
+echo "* * * * * /bin/bash -l -c '/mnt/avalon/ingest_rights.sh' >/dev/null 2>&1" >> /var/spool/cron/crontabs/root
+echo "* * * * * /bin/bash -l -c '/mnt/avalon/dropbox_rights.sh' >/dev/null 2>&1" >> /var/spool/cron/crontabs/root
+
+chmod 600 /var/spool/cron/crontabs/root
+service cron reload
+
 cd /home/app/avalon
 su -m -c "bundle exec rake db:migrate" app
