b471d061fc8ae6d906d1bb0782be99b4cca570f3
Douglas Hahn <dhahn@tamu.edu>
2025-02-05 14:30:08 -0600

    nginx tweaks

--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
 # Make config file from template
-[ -z "$env_rtmp_avalon_url" ] && env_rtmp_avalon_url="http://avalon"
-[ -z "$env_avalon_streaming_port" ] && env_avalon_streaming_port=80
-[ -z "$env_master_file_location" ] && env_master_file_location=/data
-[ -z "$env_proxy_pass_ip" ] && env_proxy_pass_ip=127.0.0.1
+[ -z "$env_kaltura_streaming_port" ] && env_kaltura_streaming_port=80
+[ -z "$env_rtmp_avalon_url" ] && env_rtmp_avalon_url="http://avalon:3000"
+[ -z "$env_master_file_location" ] && env_master_file_location=/mnt/avalon/masterfiles
+[ -z "$env_proxy_pass_url" ] && env_proxy_pass_url=http://avalon:3000
+[ -z "$env_resolver_ip" ] && env_resolver_ip=127.0.0.1
 
+export env_kaltura_streaming_port
 export env_rtmp_avalon_url
-export env_avalon_streaming_port
 export env_master_file_location
-export env_proxy_pass_ip
+export env_proxy_pass_url
+export env_resolver_ip
 
-envsubst '$env_rtmp_avalon_url,$env_avalon_streaming_port,$env_master_file_location,$env_proxy_pass_ip' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '$env_rtmp_avalon_url,$env_kaltura_streaming_port,$env_master_file_location,$env_proxy_pass_url,$env_resolver_ip' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 exec /usr/local/nginx/sbin/nginx
