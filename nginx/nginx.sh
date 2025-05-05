d3f83b0240567d4c4ab4bf57eb26f3775e79a070
Douglas Hahn <dhahn@tamu.edu>
2025-05-05 09:56:45 -0500

    tweaks

--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -12,7 +12,10 @@ export env_rtmp_avalon_url
 export env_master_file_location
 export env_proxy_pass_url
 export env_resolver_ip
+export env_auth_request_line
 
-envsubst '$env_rtmp_avalon_url,$env_kaltura_streaming_port,$env_master_file_location,$env_proxy_pass_url,$env_resolver_ip' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '$env_auth_request_line,$env_rtmp_avalon_url,$env_kaltura_streaming_port,$env_master_file_location,$env_proxy_pass_url,$env_resolver_ip' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+
+sed -i "s,{{Content-Security-Policy}},${new_value},g" /etc/nginx/nginx.conf
 
 exec /usr/local/nginx/sbin/nginx
